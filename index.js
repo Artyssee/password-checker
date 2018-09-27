3d446e5d2a519659ac8b565eca55c567eb4b3405
Martijn van Halderen <martijnmdesigns@hotmail.com>
2018-09-27 13:32:52 +0200

    Javascript Form checker base setup

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-'use strict'
-
+let form = document.querySelector('.form');
 let password = document.querySelector('.password');
-let passwordInput;
+let submitbutton = document.querySelector('.form-button');
 
-password.addEventListener('input', function() {
+var FORMTEST = {
+	minCharacters: 8,
+	specialsign: RegExp(/[{}.,!#:]/),
+	numbers: RegExp(/([0-9])\w{1}/),
+	capitals: RegExp(/[A-Z]/),
+	noncapitals: RegExp(/[a-z]/),
+};
+
+
+form.addEventListener('submit', function( event ) {
+	event.preventDefault();
 	let passwordInput = password.value;
-	let passwordLength = passwordInput.length;
-
-	switch ( true ) {
-		case passwordLength < 11:
-			console.log('Min. 8 characters');
-			break;
-	
-		default:
-			console.log(passwordLength);
-			break;
-	}
+	checkForm(passwordInput);
 });
 
-
-
+function checkForm(passwordInput) {
+	console.log(passwordInput);
+	if(
+		passwordInput.length > FORMTEST.minCharacters &&
+		FORMTEST.specialsign.test(passwordInput) &&
+		FORMTEST.numbers.test(passwordInput) &&
+		FORMTEST.capitals.test(passwordInput) &&
+		FORMTEST.noncapitals.test(passwordInput)
+	) {
+		console.log('CONGRATULATIONS U PASSED');
+	} else {
+		console.log('PASSWORD NOT ACCEPTED');
+		return false;
+	};
+}
