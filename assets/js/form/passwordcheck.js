028b2f4456a7f48d140fa4d5071a15f847366ab5
Martijn van Halderen <martijnmdesigns@hotmail.com>
2018-10-04 14:11:33 +0200

    removed all vars

--- assets/js/form/passwordcheck.js
+++ assets/js/form/passwordcheck.js
@@ -4,7 +4,7 @@ let form = document.querySelector('.form');
 let password = document.querySelector('.password');
 let submitbutton = document.querySelector('.form-button');
 
-var FORMTEST = {
+const FORMTEST = {
 	minCharacters: 8,
 	specialsign: RegExp(/[{}.,!#:]/),
 	numbers: RegExp(/([0-9])\w{1}/),
